fc795cca6e3c1fb5b867d2cd5a4ab971c2596e6e
Diego Flores <dfmonroy07@gmai.com>
2024-02-14 11:00:22 -0600

    updated parameters

--- database.sql
+++ database.sql
@@ -6,6 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     balance BIGINT NOT NULL default 150000,
+    work_place VARCHAR(255) NOT NULL,
+    charge VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL,
     quiz_try BOOLEAN default true,
     updated_at TIMESTAMP,
     created_at TIMESTAMP
