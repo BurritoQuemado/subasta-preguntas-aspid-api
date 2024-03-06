0249054261f5418abbe7c908c9d5e0b0d715f356
Diego Flores <dfmonroy07@gmai.com>
2024-03-06 16:37:50 -0600

    added table inscriptions

--- database.sql
+++ database.sql
@@ -19,3 +19,25 @@ CREATE TABLE IF NOT EXISTS login (
     email text UNIQUE NOT NULL,
     hash VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS inscriptions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    fathers_name VARCHAR(255) NOT NULL,
+    mothers_name VARCHAR(255) NOT NULL,
+    charge VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    company_field VARCHAR(255) NOT NULL,
+    street VARCHAR(60) NOT NULL,
+    suburb VARCHAR(50) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    district VARCHAR(100) NOT NULL,
+    postal_code VARCHAR(8) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    website VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(15) NOT NULL,
+    fax VARCHAR(20) NOT NULL,
+    mobile VARCHAR(15) NOT NULL
+);
